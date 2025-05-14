5e37fbf2bd52e0701486e455174c8416277a270a
Mac L <mjladson@pm.me>
2025-05-15 00:18:19 +1000

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ use tokio::sync::{Mutex, RwLock};
 
 const TASK_COUNT: usize = 1000;
 const HEARTBEAT_SECS: u64 = 5;
-const TASK_COOLDOWN_MILLIS: u64 = 5;
+const TASK_COOLDOWN_MILLIS: u64 = 10;
+// Enables a single extra task which does not sleep. This may trigger the
+// bug more consistently.
+const EXTRA_TASK: bool = true;
 
 #[tokio::main]
 async fn main() {
+    println!(
+        "Starting loop with parameters; EXTRA_TASK: {EXTRA_TASK} TASKS: {TASK_COUNT}, TASK_COOLDOWN: {TASK_COOLDOWN_MILLIS}"
+    );
+
     let mut heartbeat = 0;
     let mutex = Arc::new(Mutex::new(0));
     let rwlock = Arc::new(RwLock::new(0));
 
-    for _ in 0..TASK_COUNT{
+    for _ in 0..TASK_COUNT {
         let mutex_clone = Arc::clone(&mutex);
         let rwlock_clone = Arc::clone(&rwlock);
 
@@ -34,6 +41,29 @@ async fn main() {
                 }
 
                 tokio::time::sleep(Duration::from_millis(TASK_COOLDOWN_MILLIS)).await;
+            }
+        });
+    }
+
+    if EXTRA_TASK {
+        let mutex_clone = Arc::clone(&mutex);
+        let rwlock_clone = Arc::clone(&rwlock);
+        tokio::spawn(async move {
+            loop {
+                {
+                    let mut guard = mutex_clone.lock().await;
+                    *guard += 1;
+                }
+
+                {
+                    let guard = rwlock_clone.read().await;
+                    let _ = *guard;
+                }
+
+                {
+                    let mut guard = rwlock_clone.write().await;
+                    *guard += 1;
+                }
             }
         });
     }
